d7a98f881447636ca34be6d5ecae21148b2e231c
Duke Do <50648115+Vafelz@users.noreply.github.com>
2024-01-20 12:42:35 -0500

    1.0.2: fixed TypeError

--- src/mod.ts
+++ src/mod.ts
@@ -15,17 +15,22 @@ import { Item } from "@spt-aki/models/eft/common/tables/IItem";
 import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
 import * as baseJson from "../db/base.json";
 import * as assortJson from "../db/assort.json";
+import { TraderHelper } from "./traderHelpers";
+import { FluentAssortConstructor } from "./fluentTraderAssortCreator";
+import { Money } from "@spt-aki/models/enums/Money";
 import { Traders } from "@spt-aki/models/enums/Traders";
+import { HashUtil } from "@spt-aki/utils/HashUtil";
 
 class VafelsTrader implements IPreAkiLoadMod, IPostDBLoadMod {
     mod: string;;
     logger: ILogger;
     private configServer: ConfigServer;
+
     constructor() {
         this.mod = "VAFELZ-ALLAMMO";
     }
     public preAkiLoad(container: DependencyContainer): void {
-        const logger = container.resolve<ILogger>("WinstonLogger");
+        this.logger = container.resolve<ILogger>("WinstonLogger");
 
         const preAkiModLoader: PreAkiModLoader = container.resolve<PreAkiModLoader>("PreAkiModLoader");
         const imageRouter: ImageRouter = container.resolve<ImageRouter>("ImageRouter");
@@ -39,7 +44,6 @@ class VafelsTrader implements IPreAkiLoadMod, IPostDBLoadMod {
     }
     public postDBLoad(container: DependencyContainer): void {
         this.configServer = container.resolve("ConfigServer");
-        this.ragfairConfig = this.configServer.getConfig(ConfigTypes.RAGFAIR);
 
         const logger = container.resolve("WinstonLogger");
 
@@ -51,20 +55,25 @@ class VafelsTrader implements IPreAkiLoadMod, IPostDBLoadMod {
         const tables = db.getTables();
 
         this.addTraderToDb(baseJson, tables, jsonUtil);
-        this.addTraderToLocales(tables, baseJson.name, "", baseJson.nickname, baseJson.location, "UwU");
+        this.addTraderToLocales(tables, baseJson.name, "VAFELZ", baseJson.nickname, baseJson.location, "OwO");
     }
     private registerProfileImage(preAkiModLoader: PreAkiModLoader, imageRouter: ImageRouter): void {
         const imageFilePath = `./${preAkiModLoader.getModPath(this.mod)}res`;
         imageRouter.addRoute(baseJson.avatar.replace(".jpg", ""), `${imageFilePath}/vafelz.jpg`);
     }
     private setupTraderUpdateTime(traderConfig: ITraderConfig): void {
-        const traderRefreshRecord: UpdateTime = {traderId: baseJson._id, seconds: 3600};
+        const traderRefreshRecord: UpdateTime = { traderId: baseJson._id, seconds: 3600 };
         traderConfig.updateTime.push(traderRefreshRecord);
     }
     private addTraderToDb(traderDetailsToAdd: any, tables: IDatabaseTables, jsonUtil: JsonUtil): void {
         tables.traders[traderDetailsToAdd._id] = {
             assort: jsonUtil.deserialize(jsonUtil.serialize(assortJson)) as ITraderAssort,
             base: jsonUtil.deserialize(jsonUtil.serialize(traderDetailsToAdd)) as ITraderBase
+            questassort: {
+                started: {},
+                success: {},
+                fail: {}
+            }
         }
     }
     private addTraderToLocales(tables: IDatabaseTables, fullName: string, firstName: string, nickName: string, location: string, description: string) {
